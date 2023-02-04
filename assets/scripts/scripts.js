aad16702ba34524aa68156b94b85f668291208d6
Jonathan Davis <trunten@btinternet.com>
2023-02-04 21:42:33 +0000

    added 5 day forecast

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,6 +3,7 @@ const unsplashId = "_5uHOtETr7A0g87rXm5bnFFv8z-frUbW5u3Q9d8qeAk";
 let history = JSON.parse(localStorage.getItem("searches")) || [];
 
 function getWeather (location) {
+    // const url = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`
     const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${location}&appid=${apiKey}`
     fetch(url)
         .then(res => res.json())
@@ -10,17 +11,16 @@ function getWeather (location) {
 }
 
 function updateWeather(data) {
-    // console.log(data);
     if (data.message) {
         alert(data.message);
         return;
     } 
-
     const name = data.name;
     addToSearchHistory(name);
     const { description, icon } = data.weather[0];
     const { temp, temp_min, temp_max, humidity } = data.main;
     const { speed, deg } = data.wind;
+    const { lat, lon } = data.coord;
     document.querySelector(".location").innerText = name;
     document.querySelector(".temperature-current").innerText = `${temp.toFixed(1)}°C`; //(${temp_min}, ${temp_max})`;
     document.querySelector(".temperature-max span").innerText = `${temp_max.toFixed(1)}°C`; //(${temp_min}, ${temp_max})`;
@@ -31,6 +31,22 @@ function updateWeather(data) {
     document.querySelector(".wind-direction-arrow").style.setProperty('--direction', deg + "deg")
     document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
     const dayNight = icon.slice(-1)==="d" ? "daytime" : ",nighttime";
+
+    const url = `http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`
+    fetch(url).then(res => res.json()).then(data => {
+        forecast = data.list.filter(item => item.dt_txt.includes("12:00"));
+        for (let i = 0; i < forecast.length; i++) {
+            const day = forecast[i];
+            const el = document.querySelector(".day" + i);
+            console.log(day, el);
+            let dt = new Date(day.dt * 1000);
+            console.log(dt.toLocaleDateString("en-gb", { weekday:"short", day:"numeric", month:"short"}));
+            document.querySelector(`.day${(i+1)} .date`).textContent = dt.toLocaleDateString();
+            document.querySelector(`.day${(i+1)} .temp span`).textContent = day.main.temp;
+            document.querySelector(`.day${(i+1)} .humidity span`).textContent = day.main.humidity;
+            document.querySelector(`.day${(i+1)} .wind span`).textContent = day.wind.speed;
+        }
+    });
     
     fetch (`https://api.unsplash.com/search/photos?client_id=${unsplashId}&page=1&per_page=40&orientation=landscape&query=${name.replaceAll(" ",",")},${dayNight}`)
     .then(res => res.json())
